4295fd3b70b9bc1dd56fe5a8a182a2044bc700a9
Ansu Jammeh <ansuj9@yahoo.com>
2023-10-31 01:26:53 +0000

    Header Completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from './components/Navbar';
+import Header from './components/Header';
+
+
 
 function App() {
   return (
-  
-      <Navbar />
+    <>
     
+
+      <Navbar />
+      <Header />
+    </>
   );
 }
 
