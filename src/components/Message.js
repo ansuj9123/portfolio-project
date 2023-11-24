a800f8df210ac2fceff2c480b563e2810e4cf6a4
Ansu Jammeh <ansuj9@yahoo.com>
2023-11-24 12:21:08 +0000

    completed

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+
 
 const Message = () => {
   return (
-    <div className="container bg_*">
+    <div className="container bg-light.bg-gradient">
       <h5 className="m-3 bg-warning-subtle">Message Sent</h5>
     </div>
   );
-}
+};
 
 export default Message;
